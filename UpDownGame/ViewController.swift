7c997d26d718a83219356032d510bf8641200d08
BAE <lexon1209@gmail.com>
2023-03-07 22:56:07 +0900

    Implement: reset func, label variable

--- UpDownGame/ViewController.swift
+++ UpDownGame/ViewController.swift
@@ -9,15 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    var randomValue: Int = 0
+    var tryCount: Int = 0
+    
     @IBOutlet weak var slider: UISlider!
-
+    @IBOutlet weak var tryCountLabel: UILabel!
+    @IBOutlet weak var sliderValueLabel: UILabel!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // Do. any additional setup after loading the view.
         
+        reset()
         slider.setThumbImage(UIImage(imageLiteralResourceName: "slider_thumb.png"), for: .normal)
     }
-
+    
     @IBAction func sliderValueChanged(_ sender: UISlider){
         print(sender.value)
     }
@@ -28,7 +36,11 @@ class ViewController: UIViewController {
     
     @IBAction func touchUpResetButton(_ sender: UIButton){
         print("reset")
+        reset()
     }
-
+    
+    func reset() {
+        print("reset!")
+    }
 }
 
