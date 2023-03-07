7dffe2675be9a37639d8fa47b448e13199c1f0cf
BAE <lexon1209@gmail.com>
2023-03-07 22:33:39 +0900

    Init: implement asset, component, etc

--- UpDownGame/ViewController.swift
+++ UpDownGame/ViewController.swift
@@ -8,12 +8,27 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var slider: UISlider!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        slider.setThumbImage(UIImage(imageLiteralResourceName: "slider_thumb.png"), for: .normal)
     }
 
+    @IBAction func sliderValueChanged(_ sender: UISlider){
+        print(sender.value)
+    }
+    
+    @IBAction func touchUpHitButton(_ sender: UIButton){
+        print(slider.value)
+    }
+    
+    @IBAction func touchUpResetButton(_ sender: UIButton){
+        print("reset")
+    }
 
 }
 
